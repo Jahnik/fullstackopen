080144f74a4bd032e64025b04b9363893d12b7ed
Jahnik <jahniksuriya@gmail.com>
2025-09-07 16:11:25 -0400

    Completed 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 
 
 
@@ -15,10 +15,25 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
-  const increaseGood = () => setGood(good + 1)
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const increaseBad = () => setBad(bad + 1)
+  const increaseGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(total + 1)
+  }
+
+  const increaseNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(total + 1)
+  }
+
+  const increaseBad = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(total + 1)
+  }
 
   return (
     <div>
@@ -29,6 +44,10 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <h1>Statistics</h1>
+      <p>Total Feedback: {total}</p>
+      <p>Feedback Average: {(good + (-bad))/total}</p>
+      <p>Feedback %Positive: {good/total*100}%</p>
     </div>
   )
 }
