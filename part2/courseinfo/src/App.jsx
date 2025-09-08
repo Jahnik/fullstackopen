775fcc0118f9f6823c248f12968c39284a7342dc
Jahnik <jahniksuriya@gmail.com>
2025-09-08 00:13:52 -0400

    Completed 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,36 +1,4 @@
-
-
-const Course = ({courses}) => {
-  return(
-  <div>
-    {courses.map(course => 
-    <div key={course.id}>
-      <Header header={course.name}/>
-      <Content parts={course.parts}/>
-    </div>
-    )}
-  </div>
-  )
-}
-
-const Header = ({header}) => {
-  return(
-    <h1>{header}</h1>
-  )
-}
-
-const Content = ({parts}) => {
-  return(
-    <div>
-      {parts.map(part => 
-        <p key={part.id}>
-          {part.name} {part.exercises}
-        </p>
-      )}
-      <p>Total Exercises: {parts.reduce((acc, part) => part.exercises + acc, 0)}</p>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
